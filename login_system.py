051457f48ff21d57bca46f9598a72a1aa1445a48
Alsu Askerova <askeralsu13@gmail.com>
2026-04-20 23:27:30 +0400

    Implement basic login functionality

---
+++ login_system.py
@@ -0,0 +1,6 @@
+username= input("Enter username: ")
+password= input("Enter password: ")
+if username =="Allok" and password == "1234":
+    print("Login successful")
+else:
+    print("Wrong username or password")
